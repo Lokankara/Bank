af7e965e28027fc55da5b37962d9b03f30035a49
Lokankara <poliyakpavlo@gmail.com>
2024-10-12 21:53:27 +0300

    exclude config

--- src/test/java/com/wallet/bank/BankApplicationTests.java
+++ src/test/java/com/wallet/bank/BankApplicationTests.java
@@ -1,9 +1,13 @@
 package com.wallet.bank;
 
+import com.wallet.bank.config.OpenAIRestTemplateConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest(classes = BankApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = OpenAIRestTemplateConfig.class))
 class BankApplicationTests {
 
     @Test
